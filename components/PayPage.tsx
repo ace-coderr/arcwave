4751045128ce2192015455cdb9ea98a2ea6f16be
ace <ace7coderr@gmail.com>
2026-05-03 22:27:57 +0100

    Fee on all payments - both standard and stealth

--- components/PayPage.tsx
+++ components/PayPage.tsx
@@ -52,7 +52,7 @@ function Logo() {
   return (
     <div className="pay-logo">
       <img
-        src="/conduit-logo-white.jpeg"
+        src="/conduit-logo-white.png"
         alt="Conduit"
         style={{ height: 28, width: "auto", objectFit: "contain" }}
       />
